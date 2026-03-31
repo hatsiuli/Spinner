a6fc4b5307bb51d3ce5983defb5754849b25cbff
hatsiuli <hatsiuli@gmail.com>
2026-03-31 19:28:53 +0300

    comments added

--- app/src/main/java/com/example/spinner/MainActivity.kt
+++ app/src/main/java/com/example/spinner/MainActivity.kt
@@ -12,8 +12,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    lateinit var lbl: TextView
-    lateinit var spn: Spinner
+    lateinit var lbl: TextView      // [SPINNER] wait for graphics to start
+    lateinit var spn: Spinner       // [SPINNER] wait for graphics to start
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,15 +25,17 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        lbl = findViewById<TextView>(R.id.textView)
-        lbl.text = "(nothing seletcted)"
-        spn = findViewById<Spinner>(R.id.spinner)
+        lbl = findViewById<TextView>(R.id.textView)     // [SPINNER] the label to show my selection
+        spn = findViewById<Spinner>(R.id.spinner)       // [SPINNER] the spinner
 
+        // [SPINNER] from the array of strings we made in strings.xml prepare an arrayAdapter
         val arrAdapter = ArrayAdapter.createFromResource(
             this,R.array.myList,android.R.layout.simple_spinner_item)
         arrAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        spn.adapter = arrAdapter
 
+        spn.adapter = arrAdapter    // [SPINNER] and connect it to our spinner
+
+        // [SPINNER] in case of something is selected set our label's text
         spn.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
                 val lst = getResources().getStringArray(R.array.myList)
